87e01918fca8452b0316170fc18fbf8309b64f58
Andriy <andri0667365973@gmail.com>
2025-10-07 13:49:43 +0300

    feat:tanstack provider

---
+++ src/app/(app)/(providers)/query-clinet-provider.tsx
@@ -0,0 +1,21 @@
+// src/app/(app)/providers/query-client.provider.tsx
+"use client";
+
+import { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function QueryClientProviderWrapper({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
