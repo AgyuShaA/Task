2192a2c5d8af0aefd00f7128d44ace3bc7db0169
Andriy <andri0667365973@gmail.com>
2025-10-10 13:23:40 +0300

    refactor:removed unnesesary page from sentry init

--- src/app/(client)/(app)/page.tsx
+++ src/app/(client)/(app)/page.tsx
@@ -4,9 +4,16 @@ import { Suspense } from 'react'
 import { HomeModule } from '../modules/home'
 import { postQueryOptions } from '@/client/entities/api/post'
 import { getQueryClient } from '@/pkg/libraries/rest-api/service'
+import * as Sentry from '@sentry/nextjs'
 
 export const revalidate = 30
 export const dynamic = 'force-static'
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: true,
+  tracesSampleRate: 1.0,
+})
 
 export default async function Home() {
   const queryClient = getQueryClient()
