e385b2d6f66b5de9baad7b6356a2ecf3fa21136f
Andriy <andri0667365973@gmail.com>
2025-10-10 13:12:54 +0300

    feat:sentry set up

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -3,11 +3,29 @@ import '@/config/styles/globals.css'
 import { UiProvider } from '../../pkg/libraries/ui'
 import RestApiProvider from '@/pkg/libraries/rest-api/rest-api.provider'
 
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: process.env.NODE_ENV === 'production',
+  tracesSampleRate: 1.0,
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const throwError = () => {
+    try {
+      throw new Error('🚨 Sentry test error!')
+    } catch (error) {
+      Sentry.captureException(error)
+      console.error(error)
+    }
+  }
+  throwError()
+
   return (
     <html lang='en' className='light' style={{ colorScheme: 'light' }}>
       <body className='antialiased' suppressHydrationWarning>
